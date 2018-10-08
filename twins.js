1a156003ec2eee3c2408065b1e9dc2cd1df9ceac
Asraa Mustufa <Asraa@Asraa-Mustufas-MacBook-Pro.local>
2018-10-08 17:15:14 -0500

    twins

---
+++ twins.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+	//when you click on before image, fade that image out
+
+
+$(".twinsbefore").click(function(){
+
+	$(".twinsbefore").fadeOut(1000);
+
+
+
+});
+
+$(".twinsafter").click(function(){
+
+
+$(".twinsbefore").fadeIn(1000);
+
+});
+
+//when you clickon the after image, fade the before image back in
+
+
+
+
+
+});
